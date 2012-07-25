1129bc2ee73704003d5a7466c429c028aeda4a07
Adam Stepinski <adam.stepinski@gmail.com>
2012-07-25 16:25:08 -0700

    initial commit

---
+++ flashcards.js
@@ -0,0 +1,133 @@
+// ADD YOUR OPENMINDS APP ID HERE
+var APP_ID = '500f92bf94d94a5b13000013';
+// SPECIFY URL OF YOUR REDIRECT URL HERE
+var REDIRECT_URI = 'http://localhost/~adam/flashcards/oauth_redirect.html';
+
+// The OpenMinds API host.
+var API_ROOT = 'http://api.openminds.io';
+// Id of the list to fetch through the OpenMinds API.
+var DEFAULT_LIST_ID = '4ffb717e94d94a744b000796';
+
+
+/**
+ *
+ */
+function init() {
+  if (localStorage.getItem('omAccessToken')) {
+    om.accessToken = localStorage.getItem('omAccessToken');
+    postLogIn();
+    $('#logged-out').hide();
+  } else {
+    $('#login').show();
+    $('#logout').hide();
+    $('#app').hide();
+  }
+}
+
+
+/**
+ * Logs the user in with OpenMinds and stores the API access token
+ * in local storage. If the login is successful, start the flashcard app.
+ */
+function login() {
+  om.logIn({
+    appId: APP_ID,
+    redirectUri: REDIRECT_URI,
+    callback: function(accessToken) {
+      if (accessToken) {
+        localStorage.setItem('omAccessToken', accessToken);
+        postLogIn();
+      }
+    }
+  });
+}
+
+
+/**
+ * Logs the user out of OpenMinds and clears the access token from
+ * local storage. Shows the logged-out view of the page.
+ */
+function logout() {
+  localStorage.removeItem('omAccessToken');
+  om.logOut(function() {
+    $('#app').hide();
+    $('#logout').hide();
+    $('#logged-out').show();
+    $('#login').show();
+  });
+}
+
+
+/**
+ * Once we have an access token, fetch a list through the OpenMinds API
+ * and start the flashcard app with the list.
+ */
+function postLogIn() {
+  $('#login').hide();
+  $('#logout').show();
+  $('#logged-out').hide();
+  getList(DEFAULT_LIST_ID, function(list) {
+    startFlashcards(list);
+  });
+}
+
+
+/**
+ * Fetches the given list through the OpenMinds API.
+ * @param {String} listId the id of the list to fetch from OpenMinds.
+ * @param {Function} success The handler to call after fetching the list. The
+ *   JSON list data is passed in as the first argument to the handler.
+ */
+function getList(listId, success) {
+  $.ajax({
+    url: API_ROOT + '/0/data/lists/' + listId,
+    dataType: 'json',
+    data: {
+      'sort': 'adaptive',
+      },
+    headers: {
+      'X-OpenMinds-Access-Token': om.accessToken,
+      },
+    success: success,
+  });
+}
+
+
+/**
+ * Creates the flashcards app using the given OpenMinds list.
+ */
+function startFlashcards(list) {
+  var currentIndex = 0;
+
+  function showCurrentFlashcard() {
+    var item = list.items[currentIndex];
+    $('#word').text(item.word);
+    $('#defn').text(item.defn);
+    $('#index').text((currentIndex+1) + '/' + list.items.length);
+  }
+
+  function showNextFlashcard() {
+    currentIndex = (currentIndex + 1) % list.items.length;
+    showCurrentFlashcard();
+  }
+
+  function showPrevFlashcard() {
+    currentIndex = (currentIndex == 0) ? list.items.length - 1 : currentIndex - 1;
+    showCurrentFlashcard();
+  }
+
+  $('#next').click(showNextFlashcard);
+  $('#prev').click(showPrevFlashcard);
+  $(window).keydown(function(e) {
+    if (e.keyCode == 39) {
+      showNextFlashcard();
+    } else if (e.keyCode ==37) {
+      showPrevFlashcard();
+    }
+  });
+
+  // Render the list title and show the first flashcard.
+  $('#title').text(list.title);
+  $('#app').show();
+  showCurrentFlashcard();
+}
---
+++ openminds_connect.js
@@ -0,0 +1,78 @@
+(function() {
+
+window.om = window.om || {};
+var om = window.om;
+
+var DEFAULT_OPENMINDS_ROOT = 'http://api.openminds.io';
+var DEFAULT_LOGIN_WINDOW_WIDTH = 415;
+var DEFAULT_LOGIN_WINDOW_HEIGHT = 300;
+var LOGIN_WINDOW_POLL_DURATION = 200;
+
+/**
+ * Shows an oAuth popup window to give the app permission to access the
+ * OpenMinds API. If the user is not already signed in to OpenMinds in this
+ * browser, they will need to sign in. If they have not previously granted
+ * permissions to the app, they will need to do so next. Once the app has
+ * permissions, OpenMinds will redirect the popup window to the URL specified
+ * by the app. The API access token will be provided in the hash fragment of
+ * the URL.
+ * @param {Object} options The options for the log in call. Valid properties
+ *  include:
+ *  - appId: The OpenMinds app id.
+ *  - apiRoot: The OpenMinds API root. If not specified, the production
+ *    default is used.
+ *  - redirectUri: The URI that will be loaded in the popup window before and
+ *    after redirecting to the OpenMinds oAuth page.
+ *  - width: The widh of the popup window (optional).
+ *  - height: The widh of the popup window (optional).
+ *  - callback: The function called after the popup window closes. The API
+ *    access token is passed as the sole argument to the callback. If the user
+ *    closes the popup window or does not allow API access to the app, the
+ *    access token will be null or undefined.
+ */
+om.logIn = function(options) {
+  var root = options.apiRoot || DEFAULT_OPENMINDS_ROOT;
+  var url = root + '/dialog/oauth?';
+  if (options.appId) {
+    url += 'app_id=' + options.appId;
+  }
+  if (options.redirectUri) {
+    url += '&redirect_uri=' + encodeURIComponent(options.redirectUri);
+  }
+
+  url = options.redirectUri + '?redirect=' + encodeURIComponent(url);
+
+  var width = options.width || DEFAULT_LOGIN_WINDOW_WIDTH;
+  var height = options.height || DEFAULT_LOGIN_WINDOW_HEIGHT;
+  var loginWindow = window.open(
+      url,
+      '_blank',
+      'width=' + width + ',height=' + height);
+  window.loginWindow = loginWindow;
+  var timerId = window.setInterval(function() {
+    if (loginWindow.closed || om.windowClosed) {
+      window.clearInterval(timerId);
+      om.windowClosed = false;
+      options.callback && options.callback(om.accessToken);
+    }
+  }, LOGIN_WINDOW_POLL_DURATION);
+};
+
+
+/**
+ * Logs out the currently authenticated OpenMinds user.
+ * @param {Function} callback Called once the user is logged out.
+ */
+om.logOut = function(callback) {
+  // We're using an Image with the src URL set to the signout endpoint
+  // in OpenMinds. This hack is the easiest way to do a cross-origin
+  // request where we don't care about the response.
+  var image = new Image();
+  image.style.display = 'none';
+  image.onload = callback;
+  image.onerror = callback;
+  image.src = 'http://openminds.io/auth/signout?no_redirect=1';
+  document.body.appendChild(image);
+};
+
+})();
